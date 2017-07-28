0f7612830372db833aeaa69961f744b7a4a294d6
ZouAlan <alanchromecast2014@gmail.com>
2017-07-29 02:25:44 +0800

    add rx to demo

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/filter';
+
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+
 import { AppComponent } from './app.component';
 import { NgxfUploaderModule } from 'ngxf-uploader';
 
