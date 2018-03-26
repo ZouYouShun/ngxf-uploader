023d9b9022c741b0ea948225163c277c33ed8de1
AlanZou <e936106@gmail.com>
2018-03-26 20:46:08 +0800

    remove error file

--- src/lib/src/sample.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sample-component',
-  templateUrl: './sample.component.html',
-  styleUrls: ['./sample.component.scss']
-})
-export class SampleComponent {}
